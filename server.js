a2dc09e3abb76674ba2cefb12efb0f22b6a5e5ad
Alan Magelssen <alanmagelssen@gmail.com>
2018-11-17 15:12:00 -0800

    add public dir for css import to ejs

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.render('pages/index');
 });
