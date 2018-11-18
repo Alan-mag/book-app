62a6fedab1f0f992f52fda6520fa71d54d4c4f35
Alan Magelssen <alanmagelssen@gmail.com>
2018-11-17 16:31:21 -0800

    endpoint search book

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 'use strict';
 const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+const pg = require('pg');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -10,5 +13,53 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.render('pages/index');
 });
+
+// ROUTES
+app.get('/search', getBook);
+
+// ------------- BOOK ----- //
+function Book(data) {
+  this.title = data.volumeInfo.title;
+  this.author = data.volumeInfo.authors;
+  this.publisher = data.volumeInfo.publisher;
+  this.description = data.volumeInfo.description;
+  this.page_count = data.volumeInfo.pageCount;
+  this.category = data.volumeInfo.mainCategory;
+  this.avg_rating = data.volumeInfo.averageRating;
+  this.image = data.volumeInfo.imageLinks.small;
+  this.language = data.volumeInfo.language;
+}
+
+Book.fetch = (query) => {
+  const _URL = `https://www.googleapis.com/books/v1/volumes?q=${query.data}`;
+  return superagent.get(_URL)
+    .then(result => {
+      console.log('Got data from API');
+      if (!result.body) { throw 'No Data'; }
+      else {
+        return result.body.items.map(searchedBook => {
+          return new Book(searchedBook);
+        });
+      }
+    })
+    .catch(err => {
+      handleError(err);
+    })
+}
+
+// for when we setup db
+function getBook(req, res) {
+  Book.fetch(req).then(data => {
+    res.send(data);
+  })
+}
+
+// ERROR HANDLER //
+function handleError(err, response) {
+  console.error('ERR', err);
+  if (response) {
+    response.status(500).send('Sorry you got this error, maybe break time?');
+  }
+}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
